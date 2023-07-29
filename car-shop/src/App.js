10e00d33d94cc80d6048964ba03f4375e5fe2e4d
bogdanvladutzirbo <bogdanvladutzirbo@gmail.com>
2023-07-29 18:08:39 +0300

    update app

--- car-shop/src/App.js
+++ car-shop/src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("")
+  const [cars, setCars] = useState([{
+    "carId": "1234",
+    "mark": "Volkswagen",
+    "model": "Golf",
+    "category": "Saloon",
+    "firstRegistration": "05/2019",
+    "power": "135kW(184Hp)",
+    "cubicCapacity": "1968ccm",
+    "fuel": "Diesel",
+    "gearBox": "Automatic transmision",
+    "mileage": "69979km",
+    "transmision": "FWD",
+    "doors": "4/5",
+    "description": "Golf 7 for sale",
+    "price": 26870
+  }])
+  useEffect(() => {
+    fetch('http://localhost:3002/car-shop/cars')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setCars(data.cars);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
   return (
+    // https://medium.com/@64rohanmalo/fetch-and-display-data-from-an-api-with-react-228de56bb446
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Input value: {input}</h1>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <h1>Cars</h1>
+      <ol>
+        {cars.map((car) => {
+          return (<li key={car.carId}>
+            MarK:{car.mark}, Model:{car.model}, Category:{car.category}, FirstRegistration:{car.firstRegistration}, Fuel:{car.fuel},
+            Mileage:{car.mileage}, Price:{car.price}
+          </li>)
+        }
+        )}
+      </ol>
     </div>
   );
 }
